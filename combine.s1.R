23ddca32d26e9de554b7704d0cd94cde6821d64e
JOHN LOVELL <johntlovell@gmail.com>
2015-02-25 17:32:05 -0600

    added n argument

--- combine.s1.R
+++ combine.s1.R
@@ -1,12 +1,12 @@
 #combine 20 scanones
-combine.s1<-function(s1s){
+combine.s1<-function(s1s,n){
   s1.out<-s1s[[1]]
-  if(20>length(s1s)){
+  if(n>length(s1s)){
     for(j in 2:length(s1s)){
       s1.out<-c(s1.out,s1s[[j]])
     }
   }else{
-    for(j in 2:20){
+    for(j in 2:n){
       s1.out<-c(s1.out,s1s[[j]])
     }
   }
